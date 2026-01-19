d0079032efb0510ee195da4b8804f1bcb8d2fa7d
ianthemazer <ian.fabian@me.com>
2026-01-18 23:15:25 -0500

    completed find and findIndex exercise

---
+++ mythical-creatures.js
@@ -0,0 +1,25 @@
+const mythicalCreatures = [
+    { name: "Dragon", type: "Fire", lastSeen: "Volcano Valley" },
+    { name: "Mermaid", type: "Water", lastSeen: "Coral Caves" },
+    { name: "Unicorn", type: "Land", lastSeen: "Enchanted Forest" },
+    { name: "Griffin", type: "Air", lastSeen: "Highwind Mountains" },
+    { name: "Kraken", type: "Water", lastSeen: "Abyssal Depths" }
+];
+
+const findWaterType = mythicalCreatures.find(function (creature) {
+    return creature.type === "Water";
+});
+
+console.log(findWaterType.name);
+
+const griffinIndex = mythicalCreatures.findIndex(function (creature) {
+    return creature.name === "Griffin";
+});
+
+console.log(griffinIndex);
+
+const findEnchantedForest = mythicalCreatures.find(function (creature) {
+    return creature.lastSeen === "Enchanted Forest";
+});
+
+console.log(findEnchantedForest.name);
